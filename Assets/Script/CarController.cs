a3d867855c92326018cbd796c1a92e403197720c
Phi Bao <112589448+PhiBeo@users.noreply.github.com>
2025-05-15 12:40:24 -0700

    Init push with functional mechanic

---
+++ Assets/Script/CarController.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public enum WheelType
+{ 
+    FrontRight,
+    FrontLeft,
+    BackRight,
+    BackLeft
+}
+
+
+public class CarController : MonoBehaviour
+{
+    public float motorForce;
+    public float brakeForce;
+    public float maxSteerAngle;
+
+    [EnumNamedArray(typeof(WheelType))]
+    public WheelCollider[] wheelColliders = new WheelCollider[Enum.GetValues(typeof(WheelType)).Length];
+
+    [EnumNamedArray(typeof(WheelType))]
+    public Transform[] wheelTransforms = new Transform[Enum.GetValues(typeof(WheelType)).Length];
+
+    private float horizontalInput;
+    private float verticalInput;
+    private float currentSteerAngle;
+    private float currentBrackForce;
+    private bool isBraking;
+
+    private void Update()
+    {
+        GetInput();
+        HandleMotor();
+        HandleSteering();
+        UpdateWheels();
+    }
+
+    private void GetInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        isBraking = Input.GetKey(KeyCode.Space);
+    }
+
+    private void HandleMotor()
+    {
+        wheelColliders[(int)WheelType.FrontLeft].motorTorque = verticalInput * motorForce;
+        wheelColliders[(int)WheelType.FrontRight].motorTorque = verticalInput * motorForce;
+
+        wheelColliders[(int)WheelType.BackRight].motorTorque = verticalInput * (motorForce / 5f);
+        wheelColliders[(int)WheelType.BackLeft].motorTorque = verticalInput * (motorForce / 5f);
+
+        currentBrackForce = isBraking ? brakeForce : 0;
+        ApplyBraking();
+    }
+
+    private void ApplyBraking()
+    {
+        wheelColliders[(int)WheelType.FrontLeft].brakeTorque = currentBrackForce;
+        wheelColliders[(int)WheelType.FrontRight].brakeTorque = currentBrackForce;
+        wheelColliders[(int)WheelType.BackRight].brakeTorque = currentBrackForce;
+        wheelColliders[(int)WheelType.BackLeft].brakeTorque = currentBrackForce;
+    }
+
+    private void HandleSteering()
+    {
+        currentSteerAngle = maxSteerAngle * horizontalInput;
+
+        wheelColliders[(int)WheelType.FrontLeft].steerAngle = currentSteerAngle;
+        wheelColliders[(int)WheelType.FrontRight].steerAngle = currentSteerAngle;
+    }
+
+    private void UpdateSingleWheel(WheelCollider collider, Transform transform)
+    {
+        Vector3 pos;
+        Quaternion rot;
+
+        collider.GetWorldPose(out pos, out rot);
+        transform.position = pos;
+        transform.rotation = rot;
+    }
+
+    private void UpdateWheels()
+    {
+        for (int i = 0; i < wheelColliders.Length; i++)
+        {
+            UpdateSingleWheel(wheelColliders[i], wheelTransforms[i]);
+        }
+    }
+}
